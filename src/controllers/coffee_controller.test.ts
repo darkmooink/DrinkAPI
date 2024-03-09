5ed5ecfd1da01f28e0258f24a9e59c58d55554dc
topherwalker <git@christopherwalker.tech>
2024-03-09 09:58:51 +0000

    test: add tests for /coffee

--- src/controllers/coffee_controller.test.ts
+++ src/controllers/coffee_controller.test.ts
@@ -9,4 +9,23 @@ test('GET /coffee should return correct object', async () => {
         drinkType: 'Coffee',
         name: 'Latte',
     });
+
+    const res2 = await request(app)
+        .get('/coffee')
+        .query({coffeeName: 'Black'});
+    expect(res2.statusCode).toEqual(200);
+    expect(res2.body).toEqual({
+        drinkType: 'Coffee',
+        name: 'Black',
+    })
 });
+
+test("GET /coffee should return a latte object if no prams passed", async () =>{
+    const res = await request(app)
+        .get('/coffee')
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({
+        drinkType: 'Coffee',
+        name: 'Latte',
+    });
+})
